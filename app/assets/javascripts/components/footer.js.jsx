c5935a7907febe9e103bbaecc51ad3c7704a6222
Alex Tarkowski <alex.tarkowski@gmail.com>
2016-08-17 12:51:43 -0400

    add body width

--- app/assets/javascripts/components/footer.js.jsx
+++ app/assets/javascripts/components/footer.js.jsx
@@ -3,8 +3,8 @@ class Footer extends React.Component {
 	render() {
 		return (
 						<div>
-							<p>A simple React app built on Rails by Alex Tarkowski.  
-								<a className="source-code" href="https://github.com/alexjtark/React-fun-todo-app">
+							<p>A simple React app built on Rails by Alex Tarkowski.</p> 
+								<p><a className="source-code" href="https://github.com/alexjtark/React-fun-todo-app">
 								 Source Code Here
 								</a>
 							</p>
